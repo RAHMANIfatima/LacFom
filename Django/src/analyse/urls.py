7a23c8f10d154cc523366af77dac88563ba60539
malala-212 <rakoto.kaliana@gmail.com>
2025-04-17 15:17:41 +0200

    maj identification des individus

--- Django/src/analyse/urls.py
+++ Django/src/analyse/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import traiter_choix
+from .views import traiter_choix,analyse_resultat,afficher_importation,choix_kit
+from .marqueurs.kits_views import add_kit, delete_kit, marquers
+
 
 urlpatterns = [
     path("identification/",traiter_choix,name="traiter_choix"),
+    path("resultat/",analyse_resultat,name="analyse_resultat"),
+    path("visualisation/",afficher_importation,name="afficher_importation"),
+    path('choix-du-kit/',choix_kit,name="choix_kit"),
+    path('add-kit/', add_kit, name='add-kit'),
+    path('delete-kit/<str:kit_name>/', delete_kit, name='delete-kit'),
 ]
