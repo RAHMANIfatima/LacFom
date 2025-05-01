1c4211e84381392f5fa22a1591f30450e74d5cc2
Aliou <diallom261@gmail.com>
2025-05-01 15:02:45 +0200

    liaison avec algo

--- Django/src/Algo/temoin.py
+++ Django/src/Algo/temoin.py
@@ -1,7 +1,6 @@
 
 from Algo.individus import Individus
 
-
 class Temoin(Individus):
     """ Common informations between mother, fetus and father
 
@@ -9,7 +8,7 @@ class Temoin(Individus):
             data (dict) : markers, alleles, heights and informatif character
     """
 
-    def __init__(self, ID, data):
+    def __init__(self, ID, data, kit):
         """ The constructor for Patient class
 
             Parameters :
@@ -18,8 +17,9 @@ class Temoin(Individus):
             data (dict)
         """
         super().__init__(ID, data)
+        self.kit = kit
         self.ispositif = self.det_genre()
-        self.TPOS = {"AMEL":["X","Y"], "CSF1PO":[12],"D13S317":[9,11],"D16S539":[9,13], "D18S51":[16,18], "D21S11":[29,31.2], "D3S1358":[17,18], "D5S818":[12],"D7S820":[8,11], "D8S1179":[14,15], "FGA":[20,23], "Penta_D":[12,13], "Penta_E":[7,14], "TH01":[6,9.3], "TPOX":[11], "vWA":[16,19]} #2800M
+        self.TPOS = self.kit.get_tpos_data()  # récupération dynamique du profil TPOS
 
     def det_genre(self):
         if "POS" in self.ID.upper():
@@ -30,12 +30,22 @@ class Temoin(Individus):
             # Ni temoin positif ni negatif ... probleme
             return 6
 
+
+
+
+
     def check(self):
+        """Vérifie la concordance entre les données du témoin et les valeurs attendues."""
+        if self.ispositif == 6:
+            return self.ispositif
+
         listeFalse = []
         for marqueur in self.TPOS.keys():
-            if (self.ispositif and self.data[marqueur]['Allele'] != self.TPOS[marqueur]) or (not self.ispositif and self.data[marqueur]['Allele'] != []) :
+            allele_observe = self.data.get(marqueur, {}).get('Allele', [])
+
+            if self.ispositif and allele_observe != self.TPOS[marqueur]:
                 listeFalse.append(marqueur)
-            elif self.ispositif == 6:
-                #sys.out.write("Error: name for positive/negative control is unknown")
-                return self.ispositif
-        return listeFalse
+            elif not self.ispositif and allele_observe != []:
+                listeFalse.append(marqueur)
+
+        return listeFalse                
